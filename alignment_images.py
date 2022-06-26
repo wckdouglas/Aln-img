54b0d0f38d0877de421780f40da54dc5fed8fc49
Douglas Wu <wckdouglas@gmail.com>
2022-06-26 15:00:27 -0400

    modify function

--- alignment_images.py
+++ alignment_images.py
@@ -12,28 +12,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 import pysam
-from more_itertools import zip_equal
 from pydantic import FilePath, validate_arguments
 
 TRUNCATED_READ_DEPTH = 50
-Base = Enum("Base", "A C T G N")
 
 
-def base_encode(base_list: list[str]) -> list[int]:
-    """
-    Encode the bases in the list to integers.
+class Matrix(Enum):
+    FORWARD_BASE = 0
+    REVERSE_BASE = 1
+    INSERTION = 2
+    DELETION = 3
 
-    :param list[str] base_list: list of bases, e.g.["A", "C", "T", "G"]
-    :return: list of encoded bases, e.g. [0, 1, 2, 3]
-    :rtype: list[int]
-    """
-    return list(map(lambda x: Base[x.upper()].value, base_list))
+
+class Strand(Enum):
+    FORWARD = 0
+    REVERSE = 1
+
+
+class Base(Enum):
+    A = 0
+    C = 1
+    T = 2
+    G = 3
+    N = 4
 
 
 @validate_arguments
-def alignment_images(bam_fn: FilePath, contig: str, start: int, stop: int) -> npt.NDArray[np.int16]:
+def pileup_images(bam_fn: FilePath, ref_fa_fn: FilePath, contig: str, start: int, stop: int) -> npt.NDArray[np.float16]:
     """
-    Generate alignment images for a given region.
+    Generate pileup images for a given region, given a bam and fasta reference file
 
     :param bam: BAM file path
     :param contig: contig name
@@ -42,30 +49,65 @@ def alignment_images(bam_fn: FilePath, contig: str, start: int, stop: int) -> np
     :return: 3D image (base, qual, strand)
     :rtype: numpy.ndarray
     """
-    locus_size = stop - start
-    initial_img_shape = (locus_size, TRUNCATED_READ_DEPTH)
-    base_2d_img = np.zeros(shape=initial_img_shape, dtype=np.int8)  # this should be in the range of 0 to 3
-    strand_2d_img = np.zeros(shape=initial_img_shape, dtype=np.int8)  # this should be in the range of -1 to 1
-    qual_2d_img = np.zeros(shape=initial_img_shape, dtype=np.int8)  # this should be in the range of 33 to 93
 
-    with pysam.AlignmentFile(bam_fn, "rb") as bam:  # type: ignore # pylint: disable=no-member
-        for i, pileupcolumn in zip_equal(
-            range(locus_size),
-            bam.pileup(contig, start, stop, max_depth=TRUNCATED_READ_DEPTH, stepper="all", truncate=True),
-        ):
-            strands = [-1 if read.alignment.is_reverse else 1 for read in pileupcolumn.pileups]  # type: ignore
-            n_reads = len(strands)
-            base_2d_img[i, :n_reads] += base_encode(pileupcolumn.get_query_sequences())  # type: ignore
-            qual_2d_img[i, :n_reads] += np.array(pileupcolumn.get_query_qualities())  # type: ignore
-            strand_2d_img[i, :n_reads] += strands
-    genomic_img = np.stack([base_2d_img.T, qual_2d_img.T, strand_2d_img.T])
-    return genomic_img
+    with pysam.Samfile(bam_fn) as bam, pysam.FastaFile(ref_fa_fn) as ref_fa:
+        """
+        from pysam:
+
+        Information on match, mismatch, indel, strand, mapping
+        quality and start and end of a read are all encoded at the
+        read base column. At this column, a dot stands for a match
+        to the reference base on the forward strand, a comma for a
+        match on the reverse strand, a '>' or '<' for a reference
+        skip, `ACGTN' for a mismatch on the forward strand and
+        `acgtn' for a mismatch on the reverse strand. A pattern
+        `\+[0-9]+[ACGTNacgtn]+' indicates there is an insertion
+        between this reference position and the next reference
+        position. The length of the insertion is given by the
+        integer in the pattern, followed by the inserted
+        sequence. Similarly, a pattern `-[0-9]+[ACGTNacgtn]+'
+        represents a deletion from the reference. The deleted bases
+        will be presented as `*' in the following lines. Also at
+        the read base column, a symbol `^' marks the start of a
+        read. The ASCII of the character following `^' minus 33
+        gives the mapping quality. A symbol `$' marks the end of a
+        read segment
+        """
+        ref_sequence = ref_fa.fetch(contig, start, stop)
+
+        genomic_width = stop - start
+        tensor = np.zeros(genomic_width * len(Matrix) * len(Base)).reshape(len(Matrix), len(Base), genomic_width)
+
+        for pileup_column in bam.pileup(contig, start, stop):
+            ref_position = pileup_column.reference_pos
+
+            if stop > ref_position >= start:
+                relative_position = ref_position - start
+                ref_base = ref_sequence[relative_position]
+                bases = pileup_column.get_query_sequences(mark_matches=True, add_indels=True)
+                total_aln = len(bases)
+                add_count = 1 / total_aln
+
+                for base in bases:
+                    if "+" in base:
+                        current_base, insertion_bases = parse_insertion(base)
+                        tensor = add_base_count(tensor, current_base, relative_position, add_count)
+                        for insertion_position, insertion_base in enumerate(insertion_bases):
+                            base_index = Base[insertion_base.upper()].value
+                            position_index = insertion_position + relative_position
+                            tensor[Matrix.INSERTION.value][base_index][position_index] += add_count
+                    elif base == "*":
+                        base_index = Base[ref_base.upper()].value
+                        tensor[Matrix.DELETION.value][base_index][relative_position] += add_count
+                    elif base.upper() in Base.__members__:
+                        tensor = add_base_count(tensor, base, relative_position, add_count)
+                tensor[:, :, relative_position] *= min(total_aln, TRUNCATED_READ_DEPTH)
+    return tensor
 
 
-def plot_images(genomic_img: npt.NDArray[np.int16]) -> matplotlib.figure.Figure:
+def plot_images(genomic_img):
     """
     Plot the alignment images.
-
     :param genomic_img: 3D image (base, qual, strand)
     :return: figure object
     :rtype: matplotlib.figure.Figure
@@ -73,5 +115,8 @@ def plot_images(genomic_img: npt.NDArray[np.int16]) -> matplotlib.figure.Figure:
 
     fig = plt.figure(figsize=(10, 10))
     for i in range(genomic_img.shape[0]):
-        ax_i = fig.add_subplot(3, 1, i + 1)
-        ax_i.imshow(genomic_img[i], aspect="auto")
+        ax_i = fig.add_subplot(len(Matrix), 1, i + 1)
+        img = ax_i.imshow(genomic_img[i], aspect="auto")
+        plt.colorbar(img)
+        ax_i.set_title(list(Matrix.__members__.keys())[i])
+    fig.tight_layout()
